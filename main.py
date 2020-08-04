dd64da97786beb5fb73a9a3d02256e66727c54b9
charlicruz <ccruztorre@gmail.com>
2020-08-04 18:55:43 +0200

    integration of confusion_matrix

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import pandas as pd
 import csv
 import pandas as pd
 import pso_simple
+from sklearn.metrics import confusion_matrix
+
 
 def normalize_dataset(dataset):
     min_arr = np.amin(dataset, axis=0)
@@ -48,12 +50,12 @@ def evaluate_new_fuzzy_system(ws, data, target):
         np.fmin(
             x_memb[19]['l'],
             np.fmin(
-                x_memb[20]['m'],
+                x_memb[20]['l'],
                 np.fmin(
-                    x_memb[18]['m'],
+                    x_memb[18]['l'],
                     np.fmin(
-                        x_memb[22]['m'],
-                        x_memb[23]['s']
+                        x_memb[22]['l'],
+                        x_memb[23]['l']
                     )
                 )
             )
@@ -62,17 +64,17 @@ def evaluate_new_fuzzy_system(ws, data, target):
 
     # R1 = x1 = x2 = long and X5 = x6 = long and x9 = x10 = middle and x17 = x18 = short then Inefficient
     is_inefficient = np.fmin(
-        x_memb[0]['l'],
+        x_memb[0]['s'],
         np.fmin(
-            x_memb[1]['l'],
+            x_memb[1]['s'],
             np.fmin(
-                x_memb[4]['l'],
+                x_memb[4]['s'],
                 np.fmin(
-                    x_memb[5]['l'],
+                    x_memb[5]['s'],
                     np.fmin(
-                        x_memb[2]['m'],
+                        x_memb[2]['s'],
                         np.fmin(
-                            x_memb[7]['m'],
+                            x_memb[7]['s'],
                             np.fmin(
                                 x_memb[3]['s'],
                                 x_memb[6]['s'],
@@ -86,9 +88,9 @@ def evaluate_new_fuzzy_system(ws, data, target):
 
     # R3 = x3 = x4 = long and x11 = x12 = middle and x13 = x14 = middle and x19 = x20 = short and x21 = x22 = short then Mixt
     is_mixed = np.fmin(
-        x_memb[14]['l'],
+        x_memb[14]['m'],
         np.fmin(
-            x_memb[15]['l'],
+            x_memb[15]['m'],
             np.fmin(
                 x_memb[10]['m'],
                 np.fmin(
@@ -98,12 +100,12 @@ def evaluate_new_fuzzy_system(ws, data, target):
                         np.fmin(
                             x_memb[13]['m'],
                             np.fmin(
-                                x_memb[18]['s'],
+                                x_memb[18]['l'],
                                 np.fmin(
-                                    x_memb[19]['s'],
+                                    x_memb[19]['l'],
                                     np.fmin(
-                                        x_memb[9]['l'],
-                                        x_memb[8]['l'],
+                                        x_memb[9]['s'],
+                                        x_memb[8]['s'],
                                     )
                                 )
                             )
@@ -115,8 +117,9 @@ def evaluate_new_fuzzy_system(ws, data, target):
     )
 
     result = np.argmax([is_efficient, is_mixed, is_inefficient], axis=0)
+    print(result)
     return (result == target).mean()
-
+    print(result)
 def load_dataset(filename):
     raw_dataset = pd.read_csv(filename)
     data = normalize_dataset(raw_dataset.values[:, :-1])
@@ -125,23 +128,28 @@ def load_dataset(filename):
 
 def main():
 
-    data, target = load_dataset('t.csv')#random_example_test
-    normalized = normalize_dataset(data)
+    data, target = load_dataset('random_example_test.csv')#random_example_test con t sale 0.61
+    normalized = normalize_dataset(data)# con random sale 0.6 y 0.9 conGA
     n_features = data.shape[1]
 
     fitness = lambda ws: 1.0 - evaluate_new_fuzzy_system(ws, data, target)
+    
+    
+    ###################confusion_matrix
+    print('Confusion matrix \n',  confusion_matrix(target,))
 	# Test Fuzzy
     #ws = [0.07, 0.34, 0.48, 0.26,0.07, 0.34, 0.48, 0.26,0.07, 0.34, 0.48, 0.26,0.07, 0.34, 0.48, 0.26,0.07, 0.34, 0.48, 0.26,0.07, 0.34, 0.48, 0.26] # 95%
-
+   # ws = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
     ws = [0.993900000000000,1,0.772120000000000,0.774550000000000,0.993940000000000,1,0.781820000000000,0.793949000000000,0.357580000000000,0.363640000000000,0.260610000000000,0.254550000000000,0.212120000000000,0.248480000000000,0.278790000000000,0.242420000000000,0.125450000000000,0.121820000000000,0.00606000000000000,0.0181800000000000,0.133330000000000,0.127270000000000,0,0]    # w = [0, 0.21664307088134033, 0.445098590128248, 0.2350617110613577] # 96.6%
     Classification=1.0 - fitness(ws)
     print(Classification)
     
 
+
     record = {'GA': [], 'PSO': []}
-    for _ in tqdm(range(10)):
+    for _ in tqdm(range(20)):
 		# GA
-        t, fbest = genetic_algorithm(fitness_func=fitness, dim=n_features, n_individuals=10, epochs=40, verbose=False)
+        t, fbest = genetic_algorithm(fitness_func=fitness, dim=n_features, n_individuals=20, epochs=40, verbose=False)
         record['GA'].append(1.0 - fbest)
 		# PSO
         initial=[0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5]
@@ -167,8 +175,9 @@ def main():
     ax.set_xlabel('Comparison')
     ax.set_ylabel('Accuracy')
     plt.tight_layout()
-    plt.show()
-
+    #plt.show()
+    plt.savefig("ga.pdf")
+    
 # import numpy as np
 # import skfuzzy as fuzz
 # from skfuzzy import control as ctrl
@@ -190,6 +199,5 @@ def main():
 # tip['efficient'] = fuzz.trimf(tip.universe, [13, 25, 25])
 # # You can see how these look with .view()
 # quality['average'].view()
-
 if __name__ == '__main__':
 	main()
