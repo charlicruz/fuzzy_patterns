09ad79b282b3078293005a082365f6ab22a1d13b
charlicruz <ccruztorre@gmail.com>
2020-08-08 13:45:24 +0200

    If you have to plot consusion matrix, return resulkt1 in line 131 and uncomment lines from  158 to 174

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def evaluate_new_fuzzy_system(ws, data, target):
     result = np.argmax([is_efficient, is_mixed, is_inefficient], axis=0)
    # print(result)
     return (result == target).mean()
-   # print(result)
+    
 def load_dataset(filename):
     raw_dataset = pd.read_csv(filename)
     data = normalize_dataset(raw_dataset.values[:, :-1])
@@ -145,12 +145,18 @@ def main():
     # ws = [0.993900000000000,1,0.772120000000000,0.774550000000000,0.993940000000000,1,0.781820000000000,0.793949000000000,0.357580000000000,0.363640000000000,0.260610000000000,0.254550000000000,0.212120000000000,0.248480000000000,0.278790000000000,0.242420000000000,0.125450000000000,0.121820000000000,0.00606000000000000,0.0181800000000000,0.133330000000000,0.127270000000000,0,0]    # w = [0, 0.21664307088134033, 0.445098590128248, 0.2350617110613577] # 96.6%
     ws=[27,26.8,26.85,25.1,26.35,27.38,27.68,25.06,24.1,23,22,19,19.5,18,18.5,19,19.5,16.9,16.8,16,16.2,16.1,15.9,14]
     wsx= normalize_dataset(ws)
-    # print(wsx)
+    print(wsx)
+
+    # y_true = [2, 0, 2, 2, 0, 1]
+    # y_pred = [0, 0, 2, 2, 0, 2]
+    # cm=confusion_matrix(y_true, y_pred)
+   # print('Confusion matrix \n',cm)
+
     Classification=1.0 - fitness(wsx)
     print(Classification)
-    print(target)
-    cm=confusion_matrix(target, Classification)
-    print('Confusion matrix \n',cm)
+    # print(target)
+    # cm=confusion_matrix(target, Classification)
+    # print('Confusion matrix \n',cm)
     # plt.figure(num=10)
     # cm=confusion_matrix(wsx, Classification)
     # print(confusion_matrix(wsx, Classification))
@@ -165,37 +171,37 @@ def main():
     # plt.savefig("matrix.pdf")
     
     
-    record = {'GA': [], 'PSO': []}
-    for _ in tqdm(range(20)):
-		# GA
-        t, fbest = genetic_algorithm(fitness_func=fitness, dim=n_features, n_individuals=20, epochs=40, verbose=False)
-        record['GA'].append(1.0 - fbest)
-		# PSO
-        initial=[0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5]
-        bounds=[(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1)]
-        best, fbest = pso_simple.minimize(fitness, initial, bounds, num_particles=10, maxiter=30, verbose=False)
-        record['PSO'].append(1.0 - fbest)
-#     print(t)
-    plt.plot(t)
-    #print(fbest)
- 	# Statistcs about the runs
-    print('GA:')
-    print(np.amax(record['GA']), np.amin(record['GA']))
-    print(np.mean(record['GA']), np.std(record['GA']))
+#     record = {'GA': [], 'PSO': []}
+#     for _ in tqdm(range(20)):
+# 		# GA
+#         t, fbest = genetic_algorithm(fitness_func=fitness, dim=n_features, n_individuals=20, epochs=40, verbose=False)
+#         record['GA'].append(1.0 - fbest)
+# 		# PSO
+#         initial=[0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5]
+#         bounds=[(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1),(0, 1), (0, 1), (0, 1), (0, 1)]
+#         best, fbest = pso_simple.minimize(fitness, initial, bounds, num_particles=10, maxiter=30, verbose=False)
+#         record['PSO'].append(1.0 - fbest)
+# #     print(t)
+#     plt.plot(t)
+#     #print(fbest)
+#  	# Statistcs about the runs
+#     print('GA:')
+#     print(np.amax(record['GA']), np.amin(record['GA']))
+#     print(np.mean(record['GA']), np.std(record['GA']))
 
-    #record['Classification']
-    print('PSO:')
-    print(np.amax(record['PSO']), np.amin(record['PSO']))
-    print(np.mean(record['PSO']), np.std(record['PSO']))
+#     #record['Classification']
+#     print('PSO:')
+#     print(np.amax(record['PSO']), np.amin(record['PSO']))
+#     print(np.mean(record['PSO']), np.std(record['PSO']))
     
-    fig, ax = plt.subplots(figsize=(5, 4))
-    ax.boxplot(list(record.values()), vert=True, patch_artist=True, labels=list(record.keys()))
+#     fig, ax = plt.subplots(figsize=(5, 4))
+#     ax.boxplot(list(record.values()), vert=True, patch_artist=True, labels=list(record.keys()))
 
-    ax.set_xlabel('Comparison')
-    ax.set_ylabel('Accuracy')
-    plt.tight_layout()
-    #plt.show()
-    plt.savefig("ga.pdf")
+#     ax.set_xlabel('Comparison')
+#     ax.set_ylabel('Accuracy')
+#     plt.tight_layout()
+#     #plt.show()
+#     plt.savefig("ga.pdf")
     
 # import numpy as np
 # import skfuzzy as fuzz
